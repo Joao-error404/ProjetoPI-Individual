ddefcb5984865c76803ad917ffb0d8adf4c6431d
Joao-error404 <joao.oliveiraneto@sptech.school>
2025-05-25 00:05:31 -0300

    FuncionamentoWEBDATAVIZ

--- web-data-viz/src/routes/usuarios.js
+++ web-data-viz/src/routes/usuarios.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.post("/responder", function (req, res) {
+    usuarioController.responder(req, res);
+});
+
 module.exports = router;
