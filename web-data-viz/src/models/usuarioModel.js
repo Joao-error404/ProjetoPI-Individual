faef0c7d97f67a52543edf30e3f8a1affe16ede9
Joao-error404 <joao.oliveiraneto@sptech.school>
2025-06-03 08:35:52 -0300

    Novas validações

--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -9,12 +9,11 @@ function autenticar(email, senha) {
     return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+
 function cadastrar(nome, email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+    
     var instrucaoSql = `
         INSERT INTO Usuario (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}');
     `;
@@ -32,7 +31,7 @@ async function responder(fkUsuario, acertos) {
     const resultado = await database.executar(buscaSql);
     const numeroTentativas = resultado[0].proximaTentativa;
 
-    // Insere a nova tentativa com o número correto
+   
     var instrucaoSql = `
         INSERT INTO Quiz (fkUsuario, numeroTentativas, acertos)
         VALUES ('${fkUsuario}', '${numeroTentativas}', '${acertos}');
